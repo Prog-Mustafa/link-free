65d77c344613a1eebfc3fb1554dd114b8628e69e
Prog-Mustafa <prog.romeo@gmail.com>
2024-10-24 13:12:08 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,17 @@
-module.exports = { plugins: [`@marscollective/gatsby-theme-link-free`] }
+module.exports = {
+    plugins: [
+      `@marscollective/gatsby-theme-link-free`,
+      {
+        resolve: `gatsby-plugin-postcss`,
+        options: {
+          postCssPlugins: [
+            require("postcss-import"),
+            require("postcss-nested"),
+            require("postcss-custom-properties"),
+            require("autoprefixer"),
+          ],
+        },
+      },
+    ],
+  };
+  
