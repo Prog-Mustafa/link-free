bcc2a605610539ca7858015e7f70c7ea5e5e1b38
Prog-Mustafa <prog.romeo@gmail.com>
2024-10-24 13:16:37 +0300

    ي

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,4 @@
 module.exports = {
-    plugins: [
-      `@marscollective/gatsby-theme-link-free`,
-      {
-        resolve: `gatsby-plugin-postcss`,
-        options: {
-          postCssPlugins: [
-            require("postcss-import"),
-            require("postcss-nested"),
-            require("postcss-custom-properties"),
-            require("autoprefixer"),
-          ],
-        },
-      },
-    ],
+    plugins: [`@marscollective/gatsby-theme-link-free`],
   };
   
